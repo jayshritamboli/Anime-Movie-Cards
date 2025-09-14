e4b04cc584313891adecbdbbb91bb60f9e5bd868
jayshritamboli <jayshri.ptamboli@gmail.com>
2025-09-14 11:35:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const cl = console.log;
+
+
+let result =''
+
+anime.forEach(movie =>{
+	result	+= `		
+			<div class="col-md-4 mb-4">
+				<div class="card">
+					<figure class="movieCard">
+						<img src=${movie.image}
+						title=${movie.title}
+						alt=${movie.title}>
+						<figcaption>
+							<div class="titleInfo">
+								<div class="row">
+									<div class="col-10">
+									<h2>${movie.title}</h2>
+									</div>
+									<div class="co-2">
+									<h3><span class="badge badge-* badge-success">${movie.rating}</span></h3>
+									</div>
+								</div>
+							</div>
+							<div class="overview">
+								<h5>${movie.title}<h5>
+								<p>${movie.overview}</p>
+							</div>
+						</figcaption>
+					</figure>
+				</div>
+			</div>`
+})
+
+movieContainer.innerHTML = result;
